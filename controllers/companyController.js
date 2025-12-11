b408ec83274a3d795869598e51ea73d0125af3ab
ValeriiKorchahin <83233179+ValeriiKorchahin@users.noreply.github.com>
2025-12-11 22:32:19 +0200

    added company controller and service

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -1,5 +1,20 @@
 import { companySchema } from '../validators/companyValidator.js';
 import { BadRequestError } from '../utils/errors/BadRequestError.js';
+import { getAllCompanies } from '../services/companyService.js';
+
+export const getCompanies = async(req, res, next) => {
+    try {
+        const companies = await getAllCompanies({
+            organizationId: req.user.organizationId,
+            search: req.query.search,
+            limit: req.body.limit,
+            page: req.query.page,
+        });
+        return res.status(200).json(companies);
+    } catch(err) {
+        next(err);
+    }
+};
 
 export const createCompany = async(req, res, next) => {
     try {
--- services/companyService.js
+++ services/companyService.js
@@ -1,8 +1,34 @@
-// import { sequelize } from '../database/database.js';
-// import { CompanyModel } from '../models/index.js';
-// import { Op } from 'sequelize';
-// import { ConflictError } from '../utils/errors/ConflictError.js';
-//
+import { sequelize } from '../database/database.js';
+import { CompanyModel } from '../models/index.js';
+import { Op } from 'sequelize';
+import { ConflictError } from '../utils/errors/ConflictError.js';
+
+export const getAllCompanies = async(payload) => {
+    const { organizationId, search, limit, page } = payload;
+
+    const offset = (page - 1) * limit;
+
+    const where = {
+        organizationId: organizationId,
+        ...(search && {
+            name: { [Op.like]: `%${search}%` },
+        }),
+    };
+
+    const { rows: companies, count: total } = await CompanyModel.findAndCountAll({
+        where,
+        limit,
+        offset,
+        order: [['createdAt', 'DESC']], 
+    });
+    return {
+        data: companies,
+        total,
+        page,
+        limit,
+    };
+};
+
 // export const createCompany = async(value) => {
 //     const transaction = await sequelize.transaction();
 //     const { email, name } = value;
