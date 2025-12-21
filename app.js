87cd082ebb28c7c80904373dc9f4797c5a13ee7a
ValeriiKorchahin <83233179+ValeriiKorchahin@users.noreply.github.com>
2025-12-21 14:30:25 +0200

    added cors config

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import authRoutes from './routes/authRoutes.js';
 import companyRoutes from './routes/companyRoutes.js';
 import userRoutes from './routes/userRoutes.js';
-
+import { corsConfig } from './utils/cors/cors.js';
 import dotenv from 'dotenv';
 import './models/index.js';
 import { errorMiddleWare } from './middlewares/errorMiddleware.js';
@@ -11,9 +11,14 @@ dotenv.config({ debug: true });
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// CORS
+app.use(corsConfig());
+
+// PARSERS
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// ROUTES
 app.use('/auth', authRoutes);
 app.use('/companies', companyRoutes);
 app.use('/users', userRoutes);
